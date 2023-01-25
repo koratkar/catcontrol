c57d91e2aaf54ba4e8a49267b6efe6834e4fe3b3
Ishaan Koratkar <48111126+koratkar@users.noreply.github.com>
2023-01-25 13:54:01 -0600

    Update control.py

--- control.py
+++ control.py
@@ -10,6 +10,23 @@ servo_2 = AngularServo(17, min_angle=0, max_angle=180) # bottom servo
 
 cam = cv2.VideoCapture(0)
 
+def coord_to_x_angle(x):
+    return round(180 - (x * (180 / 1080)))
+
+def coord_to_y_angle(y):
+    return round(180 - (y * (180 / 720)))
+
+def set_angle_choppy(x, y):
+    x_old = servo_1.angle
+    y_old = servo_2.angle
+    x_diff = abs(x - x_old)
+    y_diff = abs(y - y_old)
+    for i in range(10,1,-1):
+        servo_1.angle = coord_to_x(x_old + round(x_diff / i))
+        servo_2.angle = coord_to_y(y_old + round(y_diff / i))
+    
+    
+
 urls = (
     '/', 'index',
     '/coords?', 'coords',
@@ -26,8 +43,9 @@ class coords:
       user_data = web.input()
       x = user_data.x
       y = user_data.y
-      servo_1.angle = round(180 - (x * (180 / 1080)))
-      servo_2.angle = round(180 - (y * (180 / 720)))
+      
+      set_angle_choppy(x, y)
+      
     except:
       return("no data")
 
